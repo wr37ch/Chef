b6be6026c8e9fa2823507d68d477f1f68a61be11
root <root@ip-172-31-30-177.eu-west-1.compute.internal>
2018-03-01 10:05:42 +0000

    asd

--- cookbooks/apache/test/recipes/default_test.rb
+++ cookbooks/apache/test/recipes/default_test.rb
@@ -6,13 +6,17 @@
 # found at https://docs.chef.io/inspec_reference.html
 
 unless os.windows?
-  describe user('root') do
+  describe user('ec2-user') do
     it { should exist }
     skip 'This is an example test, replace with your own test.'
   end
 end
 
 describe port(80) do
-  it { should_not be_listening }
+  it { should be_listening }
   skip 'This is an example test, replace with your own test.'
+end
+
+describe command('curl localhost') do
+  its(:stdout) {should match(/morning from chef!/) }
 end
